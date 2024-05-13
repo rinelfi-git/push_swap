8aa985cb01d594e2f9480542b918efb6ace198bf
Elie Fidele Rijaniaina <erijania@c1r5s4.42antananarivo.mg>
2024-05-13 16:20:57 +0300

    norminette

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: erijania <erijania@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 15:09:54 by erijania          #+#    #+#             */
-/*   Updated: 2024/05/13 16:03:42 by erijania         ###   ########.fr       */
+/*   Updated: 2024/05/13 16:20:45 by erijania         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,24 @@
 #include "libs/ft_printf/ft_printf.h"
 #include <stdlib.h>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    t_list  **a;
-    t_list  *c;
-    int     ci;
+	t_list	**a;
+	t_list	*c;
+	int		ci;
 
-    if (argc <= 1)
-        return (1);
-    ci = 1;
-    a = 0;
-    a = (t_list **) malloc(sizeof(t_list *));
-    while (ci < argc)
-    {
-        ft_lstadd_back(a, ft_lstnew(argv[ci]));
-        ci++;
-    }
-    c = *a;
-    while(c)
-    {
-        ft_printf("%d\n", ft_atoi(c->content));
-        c = c->next;
-    }
-    return (0);
+	if (argc <= 1)
+		return (1);
+	ci = 1;
+	a = 0;
+	a = (t_list **) malloc(sizeof(t_list *));
+	while (ci < argc)
+		ft_lstadd_back(a, ft_lstnew(argv[ci++]));
+	c = *a;
+	while (c)
+	{
+		ft_printf("%d\n", ft_atoi(c->content));
+		c = c->next;
+	}
+	return (0);
 }
