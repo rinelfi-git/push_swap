febbda17bfe6f77ed43b0e2bc790e55bdf70bddb
Elie Fidele Rijaniaina <erijania@c1r5s4.42antananarivo.mg>
2024-05-13 17:37:19 +0300

    operation in makefile

--- Makefile
+++ Makefile
@@ -3,6 +3,12 @@ CFLAGS = -Werror -Wextra -Wall
 NAME = push_swap
 DEL = rm -rf
 
+OPERATIONS =	push.c \
+				reverse_rotate.c \
+				rotate.c \
+				swap.c
+OPERATIONS_O = $(OPERATIONS:.c=.o)
+
 PRINTF_D = ./libs/ft_printf
 PRINTF_A = ftprintf
 PRINTF_H = $(PRINTF_D)/ft_printf.h
@@ -15,11 +21,14 @@ FT_LIB = $(FT_D)/libft.a
 
 all	: $(NAME)
 
-$(NAME): push_swap.c $(PRINTF_LIB) $(FT_LIB)
-	$(CC) $(CFLAGS) push_swap.c -L$(FT_D) -l$(FT_A) -L$(PRINTF_D) -l$(PRINTF_A) -o $(NAME)
+$(NAME): push_swap.c $(OPERATIONS_O) $(PRINTF_LIB) $(FT_LIB)
+	$(CC) $(CFLAGS) push_swap.c $(OPERATIONS_O) -L$(FT_D) -l$(FT_A) -L$(PRINTF_D) -l$(PRINTF_A) -o $(NAME)
 
 $(PRINTF_LIB) : $(PRINTF_H)
 	make -C $(PRINTF_D)
+
+%.o					: %.c
+	@$(CC) $(CFLAGS) -o $@ -c $<
 
 $(FT_LIB) : $(FT_H)
 	make -C $(FT_D) bonus
--- swap.c
+++ swap.c
@@ -6,7 +6,7 @@
 /*   By: erijania <erijania@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 17:16:12 by erijania          #+#    #+#             */
-/*   Updated: 2024/05/13 17:18:15 by erijania         ###   ########.fr       */
+/*   Updated: 2024/05/13 17:33:52 by erijania         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 void    swap(t_list **lst)
 {
-    
+    t_list  *current;
+    t_list  *next;
+
+    if (!lst || ft_lstsize(*lst) <= 1)
+        return ;
+    current = *lst;
+    next = current->next;
+    current->next = next->next;
+    *lst = next;
+    next->next = current;
 }
