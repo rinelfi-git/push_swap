4b669781101a3674d9a918157bc4542f0e034ac5
Elie Fidele Rijaniaina <erijania@c1r5s4.42antananarivo.mg>
2024-05-14 16:56:53 +0300

    norminette

--- swap.c
+++ swap.c
@@ -6,7 +6,7 @@
 /*   By: erijania <erijania@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 17:16:12 by erijania          #+#    #+#             */
-/*   Updated: 2024/05/14 15:10:22 by erijania         ###   ########.fr       */
+/*   Updated: 2024/05/14 16:55:31 by erijania         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	swap(t_arraylist **lst)
 {
-	t_arraylist	*current;
-	t_arraylist	*next;
+	t_arraylist	*first;
+	t_arraylist	*second;
 
 	if (arraylist_length(lst) <= 1)
 		return ;
-	current = *lst;
-	next = current->next;
-	current->next = next->next;
-	*lst = next;
-	next->next = current;
+	first = *lst;
+	second = first->next;
+	first->next = second->next;
+	*lst = second;
+	second->next = first;
 }
