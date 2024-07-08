6218f137b2cc229dd27e8cdf10709711ece6eb4f
Elie Fidele Rijaniaina <erijania@c1r5s4.42antananarivo.mg>
2024-07-08 14:21:15 +0300

    numeric check

--- main.d/utils.d/ft_isnumeric.c
+++ main.d/utils.d/ft_isnumeric.c
@@ -6,24 +6,25 @@
 /*   By: erijania <erijania@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 13:34:58 by erijania          #+#    #+#             */
-/*   Updated: 2024/05/27 17:16:37 by erijania         ###   ########.fr       */
+/*   Updated: 2024/07/08 14:20:09 by erijania         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../libs/libft/libft.h"
 
-int	ft_isnumeric(char *str)
+static int	ft_isnumber(char *str)
 {
-	int	is_sign;
-
 	while (*str)
 	{
-		is_sign = *str == '+' || *str == '-';
-		is_sign = is_sign && !ft_isdigit(*(str + 1));
-		is_sign = is_sign || (!is_sign && !ft_isdigit(*str));
-		if (is_sign)
+		if (!ft_isdigit(*(str++)))
 			return (0);
-		str++;
 	}
 	return (1);
+}
+
+int	ft_isnumeric(char *str)
+{
+	if (*str == '-' || *str == '+')
+		str++;
+	return (ft_isnumber(str));
 }
